f92fc23a23357278188ef393597267fe4c64c173
ruten <ruten@192.168.0.54>
2018-06-10 00:04:45 +0200

    updating input mask + starting save function

--- index.php
+++ index.php
@@ -14,57 +14,35 @@ and open the template in the editor.
         <link rel="stylesheet" href="//apps.bdimg.com/libs/jqueryui/1.10.4/css/jquery-ui.min.css">
         <script src="//apps.bdimg.com/libs/jquery/1.10.2/jquery.min.js"></script>
         <script src="//apps.bdimg.com/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
-         <script src="function.js"></script>
-        <link rel="stylesheet" href="jqueryui/style.css">
-        
-        
-        <script>  
-        $(function() {
-          $( "#datepicker" ).datepicker({
-            prevText: '&#x3c;zurück', prevStatus: '',
-            prevJumpText: '&#x3c;&#x3c;', prevJumpStatus: '',
-            nextText: 'Vor&#x3e;', nextStatus: '',
-            nextJumpText: '&#x3e;&#x3e;', nextJumpStatus: '',
-            currentText: 'heute', currentStatus: '',
-            todayText: 'heute', todayStatus: '',
-            clearText: '-', clearStatus: '',
-            closeText: 'schließen', closeStatus: '',
-            monthNames: ['Januar','Februar','März','April','Mai','Juni',
-            'Juli','August','September','Oktober','November','Dezember'],
-           
-            showMonthAfterYear: false,
-            showOn: 'both',
-            dateFormat:'d MM, yy'
-    } );
-        });
-        </script>
+        <script src="function.js"></script>
+        <link rel="stylesheet" href="jqueryui/style.css"> 
     </head>
     <body>
     <div class="container">
         <div class="col-sm-4">
-            <form action="/Banner_Rotation/save_banner_details.php" id="form1">
+            <form action="/Banner_Rotation/save_banner_details.php" method="post" id="form1">
             
             <div class="form-group">
                 <label>Banner-Bezeichnung</label>
                 <input  type="text" class="form-control" name="banner_bez" placeholder="Bitte  Bannername etwas  eingeben...">
             </div>
-            
-          
+                
+                       
             <div class="form-group">
                 <label>Banner-Upload</label>
-               <input id="input-b1" name="input-b1" type="file" class="file">
+               <input name="input-b1" type="file" class="file">
             </div>
 
            
             <div class="form-group">
                 <label>Aktiv-Von:</label>
-                <input class="form-control" type="date" value="" id="aktiv_von">
+                <input class="form-control" type="date" value="" name="aktiv_von">
             </div>
             
             
             <div class="form-group">
                 <label>Aktiv-Bis:</label>
-                <input class="form-control" type="date" value="" id="aktiv_bis">
+                <input class="form-control" type="date" value="" name="aktiv_bis">
             </div>
                       
                  
--- save_banner_details.php
+++ save_banner_details.php
@@ -1,5 +1,32 @@
 <?php
 
-$test = $_GET["banner_bez"];
-echo $test;
+$banner = $_POST["banner_bez"];
+$b_from = $_POST["aktiv_von"];
+$b_to = $_POST["aktiv_bis"];
 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "t_banner_rotation";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+$sql = "INSERT INTO T_BANNER (B_DESCRIPTION, B_FROM, B_TO)
+VALUES ('$banner', '$b_from', '$b_to')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
